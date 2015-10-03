47d83d3a77d64f390f2f64f3cc6a759d8a4f0fa5
bossato <recoba0110@gmail.com>
2015-10-03 20:09:16 +0900

    Modified layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,21 @@
   <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
+  <header>
+    <h1>L5Blog</h1>
+    <nav>
+      <ul>
+        <li><a href="/">一覧</a></li>
+        <li><a href="/articles/create">新規作成</a></li>
+      </ul>
+    </nav>
+  </header>
+
   <div class="container">
       <div class="row">
-          <h1>L5Blog</h1>
-          <div class="col-md-12">
-              @yield('content')
-          </div>
+        <div class="col-md-12">
+          @yield('content')
+        </div>
       </div>
   </div>
 </body>
